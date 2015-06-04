ed3cf9e1573d5b80b576cb305db98b781aa4e2b6
LucasMessiasPG <lucasmessias.pg@outlook.com>
2015-06-04 16:20:33 -0300

    up

--- locadora/resources/views/veiculo/create.blade.php
+++ locadora/resources/views/veiculo/create.blade.php
@@ -1,7 +1,45 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: messias
- * Date: 04/06/15
- * Time: 15:58
- */
+@extends('master')
+@section('conteudo')
+    @include('navbar')
+    <div class="container">
+        <a class="btn grey wave-effect" href="{{ route('painel.index') }}">Voltar</a>
+        {!! Form::open(['route' => 'veiculo.store']) !!}
+
+            <!-- Form modelo Input -->
+            <div class='form-group'>
+                {!! Form::label('modelo','Modelo:') !!}
+                {!! Form::select('modelo', [],null, ['class' => 'browser-default']) !!}
+            </div>
+
+            <!-- Form cor Input -->
+            <div class='form-group'>
+                {!! Form::label('cor','Cor:') !!}
+                {!! Form::select('cor', [],null, ['class' => 'browser-default']) !!}
+            </div>
+
+            <!-- Form placa Input -->
+            <div class='form-group'>
+                {!! Form::label('placa','Placa:') !!}
+                {!! Form::text('placa', null, ['class' => 'form-control']) !!}
+            </div>
+
+            <!-- Form ano_fabricacao Input -->
+            <div class='form-group'>
+                {!! Form::label('ano_fabricacao','Ano Fabricação:') !!}
+                {!! Form::text('ano_fabricacao', null, ['class' => 'form-control']) !!}
+            </div>
+
+            <!-- Form ano_modelo Input -->
+            <div class='form-group'>
+                {!! Form::label('ano_modelo','Ano Modelo:') !!}
+                {!! Form::text('ano_modelo', null, ['class' => 'form-control']) !!}
+            </div>
+
+            <!-- End Form -->
+            <div class='form-group'>
+                {!! Form::submit('Cadastrar',['class' => 'btn btn-success btn-block']) !!}
+            </div>
+
+        {!! Form::close() !!}
+    </div>
+@stop
